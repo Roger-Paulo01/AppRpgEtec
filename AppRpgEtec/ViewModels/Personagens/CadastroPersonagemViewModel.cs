1bd31e280021fdfbf8eda0113fff2badb8fd1e5d
Roger-Paulo01 <rel.cliente.r@gmail.com>
2026-04-15 22:28:12 -0300

    Inciar Passo-6

---
+++ AppRpgEtec/ViewModels/Personagens/CadastroPersonagemViewModel.cs
@@ -0,0 +1,138 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class CadastroPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public CadastroPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+        }
+
+        private int id;
+        private string nome;
+        private int pontosVida;
+        private int forca;
+        private int defesa;
+        private int inteligencia;
+        private int disputas;
+        private int vitorias;
+        private int derrotas;
+
+        private int teste;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nome
+        {
+            get => nome;
+            set
+            {
+                nome = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int PontosVida
+        {
+            get => pontosVida;
+            set
+            {
+                pontosVida = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Forca
+        {
+            get => forca;
+            set
+            {
+                forca = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Defesa
+        {
+            get => defesa;
+            set
+            {
+                defesa = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Inteligencia
+        {
+            get => inteligencia;
+            set
+            {
+                inteligencia = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Disputas
+        {
+            get => disputas;
+            set
+            {
+                disputas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Vitorias
+        {
+            get => vitorias;
+            set
+            {
+                vitorias = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Derrotas
+        {
+            get => derrotas;
+            set
+            {
+                derrotas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<TipoClasse> listaTiposClasse;
+
+        public ObservableCollection<TipoClasse> ListaTipoClasse
+        {
+            get {  return listaTiposClasse; }
+            set
+            {
+                if (value != null)
+                {
+                    listaTiposClasse = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+    }
+}
